678932c1bf9687ddc0ca3d8a68c2c11221672f9d
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-11 09:16:44 -0300

    state monads in ocaml

--- main.ml
+++ main.ml
@@ -77,3 +77,49 @@ let push state =
   | Locked -> (Tut, Locked) 
   | Unlocked -> (Open, Locked)
 
+module type MONAD =
+sig 
+    type 'a t
+    val bind : 'a t -> ('a -> 'b t) -> 'b t
+    val return : 'a -> 'a t
+end
+
+module type STATE = 
+sig
+    type t
+    val empty : t
+end
+
+module type STATE_MONAD =
+functor(State: STATE) ->
+sig
+    include MONAD
+    val access : 'a t -> 'a
+    val put    : State.t -> unit t
+    val get    : State.t t
+    val eval   : 'a * 'b -> 'a 
+    val exec   : 'a * 'b -> 'b
+end
+
+module STATEMONAD: STATE_MONAD = 
+functor(State : STATE) ->
+struct
+    type state = State.t
+    type 'a t = state -> ('a * state)
+    let bind m f = fun s -> match m s with | (x, s') -> f x s'        
+    let return a = fun s -> (a, s)
+    let access m = 
+        match m State.empty with
+        | (x, _) -> x
+    let put s = fun _ -> ((), s)
+    let get = fun s -> (s, s)
+    (* TODO *)
+    let eval = fun s -> fst s
+    let exec = fun s -> snd s
+end
+
+
+
+
+
+
